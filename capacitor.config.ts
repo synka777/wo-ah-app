c497e81328d7c261f939ac71a5eca8f8e1df4ab6
synka777 <m.barbegayet@gmail.com>
2025-01-29 19:02:41 +0100

    Project init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'wo-ah-app',
+  webDir: 'www'
+};
+
+export default config;
